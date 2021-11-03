8de0a29d5dd1d61de06d89b40cb0ed9ab53d9a97
David <davidcm097@gmail.com>
2021-11-03 22:21:12 +0000

    Update Controller.java

--- src/main/java/dad/calculadorafxml/Controller.java
+++ src/main/java/dad/calculadorafxml/Controller.java
@@ -4,6 +4,7 @@ package dad.calculadorafxml;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 	import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,8 +15,8 @@ import javafx.scene.control.Button;
 
 	public class Controller implements Initializable{
 		//Model
-		
     	Calculadora calculadora=new Calculadora();
+    	SimpleStringProperty visualizar = new SimpleStringProperty();
 		//View
 		public Controller() throws IOException {
 			FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
@@ -26,8 +27,7 @@ import javafx.scene.control.Button;
 
 		@Override
 		public void initialize(URL location, ResourceBundle resources) {
-			//visualizarText.textProperty().bind(pantalla);
-			
+			visualizarText.textProperty().bind(visualizar);
 		}
 		
 	    @FXML
@@ -93,114 +93,132 @@ import javafx.scene.control.Button;
 	    @FXML
 	    void CeButOnAction(ActionEvent event) {
 	    	calculadora.borrarTodo();
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void cButOnAction(ActionEvent event) {
 	    	calculadora.borrar();
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void ceroButOnAction(ActionEvent event) {
 	    	calculadora.insertar('0');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 	    
 	    @FXML
 	    void oneButOnAction(ActionEvent event) {
 	    	calculadora.insertar('1');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 	    
 	    @FXML
 	    void twoButOnAction(ActionEvent event) {
 	    	calculadora.insertar('2');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void threeButOnAction(ActionEvent event) {
 	    	calculadora.insertar('3');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 	    
 	    @FXML
 	    void fourButOnAction(ActionEvent event) {
 	    	calculadora.insertar('4');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 	    
 	    @FXML
 	    void fiveButOnAction(ActionEvent event) {
 	    	calculadora.insertar('5');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 	    
 	    @FXML
 	    void sixButOnAction(ActionEvent event) {
 	    	calculadora.insertar('6');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void sevenButOnAction(ActionEvent event) {
 	    	calculadora.insertar('7');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void eightButOnAction(ActionEvent event) {
 	    	calculadora.insertar('8');
-	    	visualizarText.setText(calculadora.getPantalla());
-	    }
-	    
+	    	visualizar.set(calculadora.getPantalla());
+	    }  
+	  
 	    @FXML
 	    void nineButOnAction(ActionEvent event) {
 	    	calculadora.insertar('9');
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla()); 
 	    }
 	    
 	    @FXML
 	    void divideButOnAction(ActionEvent event) {
 	    	calculadora.operar(Calculadora.DIVIDIR);;
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 
 	    @FXML
 	    void equalsButOnAction(ActionEvent event) {
 	    	calculadora.operar(Calculadora.IGUAL);
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void minusButOnAction(ActionEvent event) {
 	    	calculadora.operar(Calculadora.RESTAR);
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void multiplyButOnAction(ActionEvent event) {
 	    	calculadora.operar(Calculadora.MULTIPLICAR);
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void plusButOnAction(ActionEvent event) {
 	    	calculadora.operar(Calculadora.SUMAR);
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 	    @FXML
 	    void pointButOnAction(ActionEvent event) {
 	    	calculadora.insertarComa();
-	    	visualizarText.setText(calculadora.getPantalla());
+	    	visualizar.set(calculadora.getPantalla());
 	    }
 
 
 	    public GridPane getGridpa() {
 			return gridpa;
 		}
+
+
+		public final SimpleStringProperty visualizarProperty() {
+			return this.visualizar;
+		}
+		
+
+
+		public final String getVisualizar() {
+			return this.visualizarProperty().get();
+		}
+		
+
+
+		public final void setVisualizar(final String visualizar) {
+			this.visualizarProperty().set(visualizar);
+		}
+		
 	}
